add3432952cb71625911be6587af8c6c22e9cd52
YourName <it23410718@my.sliit.lk>
2025-08-29 16:18:54 +0530

    Delete all files

--- agrilink-backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getProducts, createProduct } from "../controllers/productController.js"; // 👈 add .js
-
-const router = express.Router();
-
-router.get("/", getProducts);
-router.post("/", createProduct);
-
-export default router;
