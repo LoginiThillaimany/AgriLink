71ba1a177fbbffb86dfbbf0310c85bad0a7a21af
ThirishnaviPushparajah <thirishnavi@smartzi.com>
2025-10-05 15:51:36 +0530

    Your commit message describing the changes

--- agrilink-frontend/app/_layout.js
+++ agrilink-frontend/app/_layout.js
@@ -31,7 +31,7 @@ export default function RootLayout() {
           name="signup" 
           options={{ 
             title: 'Sign Up',
-            presentation: 'modal'
+            presentation: 'modal'  
           }} 
         />
 
